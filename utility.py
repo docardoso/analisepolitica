f9f6146e80536f7304350e3286e5e18ccb600807
Guilherme Guimarães <guilherme.g.v.l.silva@gmail.com>
2017-11-11 17:06:15 -0200

    correção tabela filia, adição de mais funções
    funções N_filia e N_filia_partido

--- utility.py
+++ utility.py
@@ -13,6 +13,42 @@ def filia(parlamentar):
 
 	for i, info in enumerate(filia [1:],1): 
 		if info[1] != filia[i-1][1]: 
-			res.add(info)
+			res.add(filia[i+1])
 
+	conn.close()
 	return res
+
+
+def N_filia(parlamentar):
+	conn = SQL.connect('py_politica.db')
+	cursor = conn.cursor()
+	count = 1
+
+	
+	filia = cursor.execute('''SELECT  data, sigla, uf 
+		FROM votacao NATURAL JOIN voto 
+		WHERE id_candidato = '{}' ORDER BY data, hora'''.format(parlamentar)).fetchall()
+
+
+	for i, info in enumerate(filia [1:],1): 
+		if info[1] != filia[i-1][1]: 
+			count += 1
+
+	conn.close()
+	return count
+
+def N_filia_partido(partido):
+	conn = SQL.connect('py_politica.db')
+	cursor = conn.cursor()
+	filia_parlamentar = 0
+
+	parlamentares = cursor.execute('''SELECT DISTINCT id_candidato 
+		FROM voto NATURAL JOIN votacao 
+		WHERE sigla = '{}' '''.format(partido)).fetchall()
+
+
+	for parlamentar in parlamentares:
+		filia_parlamentar += N_filia(parlamentar[0])
+
+	return filia_parlamentar
+
