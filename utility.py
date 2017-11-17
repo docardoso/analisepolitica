648ee13ddcd291f9f07cffad02aedf1e64e7ea28
Gabriel Nascimento <gabriel.amorim.nascimento@gmail.com>
2017-11-17 11:18:59 -0200

    Função get_bancada() foi movida de obtencao_dados.py para utility.py Adicionado um comentario acima da função get_bancada() explicando o que ela faz Criada uma versão simples da função bar_graph(), que deverá no futuro criar graficos de barras de forma mais personalizada Tabs substituidos por espaços

--- utility.py
+++ utility.py
@@ -1,54 +1,82 @@
-import sqlite3 as SQL 
+import sqlite3 as sql
+from matplotlib import pyplot as pp
 
 def filia(parlamentar):
-	conn = SQL.connect('py_politica.db')
-	cursor = conn.cursor()
-	res = set()
+    conn = sql.connect('py_politica.db')
+    cursor = conn.cursor()
+    res = set()
 
-	filia = cursor.execute('''SELECT  data, sigla, uf 
-		FROM votacao NATURAL JOIN voto 
-		WHERE id_candidato = '{}' ORDER BY data'''.format(parlamentar)).fetchall()
+    filia = cursor.execute('''SELECT  data, sigla, uf
+        FROM votacao NATURAL JOIN voto
+        WHERE id_candidato = '{}' ORDER BY data'''.format(parlamentar)).fetchall()
 
-	res.add(filia[0])
+    res.add(filia[0])
 
-	for i, info in enumerate(filia [1:],1): 
-		if info[1] != filia[i-1][1]: 
-			res.add(filia[i+1])
+    for i, info in enumerate(filia [1:],1):
+        if info[1] != filia[i-1][1]:
+            res.add(filia[i+1])
 
-	conn.close()
-	return res
+    conn.close()
+    return res
 
 
 def N_filia(parlamentar):
-	conn = SQL.connect('py_politica.db')
-	cursor = conn.cursor()
-	count = 1
-
-	
-	filia = cursor.execute('''SELECT  data, sigla, uf 
-		FROM votacao NATURAL JOIN voto 
-		WHERE id_candidato = '{}' ORDER BY data, hora'''.format(parlamentar)).fetchall()
+    conn = sql.connect('py_politica.db')
+    cursor = conn.cursor()
+    count = 1
 
 
-	for i, info in enumerate(filia [1:],1): 
-		if info[1] != filia[i-1][1]: 
-			count += 1
+    filia = cursor.execute('''SELECT  data, sigla, uf
+        FROM votacao NATURAL JOIN voto
+        WHERE id_candidato = '{}' ORDER BY data, hora'''.format(parlamentar)).fetchall()
 
-	conn.close()
-	return count
+
+    for i, info in enumerate(filia [1:],1):
+        if info[1] != filia[i-1][1]:
+            count += 1
+
+    conn.close()
+    return count
 
 def N_filia_partido(partido):
-	conn = SQL.connect('py_politica.db')
-	cursor = conn.cursor()
-	filia_parlamentar = 0
+    conn = sql.connect('py_politica.db')
+    cursor = conn.cursor()
+    filia_parlamentar = 0
 
-	parlamentares = cursor.execute('''SELECT DISTINCT id_candidato 
-		FROM voto NATURAL JOIN votacao 
-		WHERE sigla = '{}' '''.format(partido)).fetchall()
+    parlamentares = cursor.execute('''SELECT DISTINCT id_candidato
+        FROM voto NATURAL JOIN votacao
+        WHERE sigla = '{}' '''.format(partido)).fetchall()
 
 
-	for parlamentar in parlamentares:
-		filia_parlamentar += N_filia(parlamentar[0])
+    for parlamentar in parlamentares:
+        filia_parlamentar += N_filia(parlamentar[0])
 
-	return filia_parlamentar
+    return filia_parlamentar
 
+# Retorna o numero de parlamentares que foram filiados a cada partido em uma das casas durante um determinado perido de tempo
+def get_bancadas(dataInicio, dataFim, casa):
+    conn = sql.connect("py_politica.db")
+    cursor = conn.cursor()
+
+    bancada = dict()
+
+    dataInicio = dataInicio.split('-')
+    dataFim = dataFim.split('-')
+
+    lista_bancada = cursor.execute('''select sigla, count(distinct voto.id_candidato) from voto
+                                    join votacao join parlamentar
+                                    where votacao.ano_mes_dia > "{}-{}-{}"
+                                    and votacao.ano_mes_dia < "{}-{}-{}"
+                                    and votacao.id_votacao=voto.id_votacao
+                                    and voto.id_candidato=parlamentar.id_candidato
+                                    and parlamentar.casa="{}"
+                                    group by sigla'''.format(dataInicio[0], dataInicio[1], dataInicio[2], dataFim[0], dataFim[1], dataFim[2], casa)).fetchall()
+
+    for i in lista_bancada:
+        bancada[i[0]] = i[1]
+
+    return bancada
+
+def bar_graph(eixo_x, eixo_y):
+    pp.bar(eixo_x, eixo_y)
+    pp.show()
