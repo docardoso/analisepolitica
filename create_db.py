daf4b289d569f940e72b4cd60c00917f6dc7841c
Guilherme Guimarães <guilherme.g.v.l.silva@gmail.com>
2018-02-07 12:24:59 -0200

    Criação das tabelas v.secreta e proposicao
    Alteração de alguns atributos

--- create_db.py
+++ create_db.py
@@ -5,28 +5,44 @@ cursor = con.cursor()
 
 cursor.execute('''
 CREATE TABLE parlamentar( 
-		id_candidato integer primary key AUTOINCREMENT,
-		id_API integer not null,
+		id_candidato varchar primary key,
 		nome varchar(50) not null,
 		casa char(2) not null);''')
 
 cursor.execute('''
-CREATE TABLE votacao( 
-		id_votacao integer primary key AUTOINCREMENT,
-		id_API varchar not null,
+CREATE TABLE proposicao( 
+		id_proposicao varchar primary key,
 		tipo varchar(10) not null,
-		data_hora datetime not null);''')
+		numero integer not null,
+		ano integer not null,
+		ementa varchar not null);''')
+
+cursor.execute('''
+CREATE TABLE votacao( 
+		id_votacao varchar primary key,
+		id_proposicao varchar not null,
+		dataHoraInicio datetime not null,
+		foreign key(id_proposicao) references proposicao(id_proposicao));''')
+
+cursor.execute('''
+CREATE TABLE votacao_secreta( 
+		id_votacao varchar not null,
+		placarSim integer not null,
+		placarNao integer not null,
+		placarAbs integer not null,
+		primary key(id_votacao),
+		foreign key(id_votacao) references votacao(id_votacao));''')
 
 cursor.execute('''
 CREATE TABLE voto( 
-		id_candidato integer not null,
+		id_candidato varchar not null,
 		id_votacao varchar not null,
 		sigla varchar not null,
 		uf char(2) not null,
 		descricao integer not null,
 		primary key(id_candidato, id_votacao),
-		foreign key (id_votacao) references votacao(id),
-		foreign key (id_candidato) references parlamentar(id));''')
+		foreign key (id_votacao) references votacao(id_votacao),
+		foreign key (id_candidato) references parlamentar(id_candidato));''')
 
 con.commit()
 con.close()
